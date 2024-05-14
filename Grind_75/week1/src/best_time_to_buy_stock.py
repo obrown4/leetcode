717392019f8d1419389a8c22a2dcbfe7b5332f06
Owen Brown <ombrown@umich.edu>
2024-05-14 10:45:08 -0400

    Bottom Up Solution for BTTBS Stock #121

--- Grind_75/week1/src/best_time_to_buy_stock.py
+++ Grind_75/week1/src/best_time_to_buy_stock.py
@@ -3,6 +3,7 @@
 #   O(n)            O(n)
 # Best Time to Buy and Sell Stock #121
 
+#Top Down
 def maxProfit(prices: list[int]) -> int:
     max_p = 0
     max_sell_price = 0
@@ -15,3 +16,16 @@ def topDownP(prices: list[int], mp: int, msp: int, n: int) -> int:
     msp = max(prices[n], msp)
     mp = max(msp - prices[n], max(mp, topDownP(prices, mp, msp, n - 1)))
     return mp
+
+#Bottom Up (Fatser Runtime)
+#STATS
+#   Runtime         Memory
+#   O(n)            O(1)
+def bottomProfit(prices: list[int]):
+    min_buy = prices[0]
+    max_sell = 0
+
+    for stock in prices[1:]:
+        max_sell = max(max_sell, stock - min_buy)
+        min_buy = min(min_buy, stock)
+    return max_sell
--- Grind_75/week1/tests/test_buy_sell_stock.py
+++ Grind_75/week1/tests/test_buy_sell_stock.py
@@ -1,17 +1,19 @@
-from ..src.best_time_to_buy_stock import maxProfit
+from ..src.best_time_to_buy_stock import bottomProfit, maxProfit
 import pytest
 
 def test_case_1():
     prices = [7,1,5,3,6,4]
     assert maxProfit(prices) == 5
+    assert bottomProfit(prices) == 5
 
 def test_case_2():
     prices = [7,6,4,3,1]
     assert maxProfit(prices) == 0
-
+    assert bottomProfit(prices) == 0
 def test_case_3():
     prices = [10, 30, 2, 3, 5]
     assert maxProfit(prices) == 20
-
+    assert bottomProfit(prices) == 20
 def test_case_4():
     assert maxProfit([1]) == 0
+    assert bottomProfit([0]) == 0
