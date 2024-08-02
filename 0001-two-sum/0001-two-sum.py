8a3bfeb68d4aad079eba8bac5b6e39e418883c19
Owen Brown <90733685+obrown4@users.noreply.github.com>
2024-08-02 19:54:19 -0400

    Time: 46 ms (98.44%), Space: 17.7 MB (26.09%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        table = {}
+        cache = {}
 
         for i, num in enumerate(nums):
-            if target - num in table:
-                return [table[target - num], i]
-            table[num] = i
-        return []
+            if target - num in cache:
+                return [cache[target - num], i]
+            cache[num] = i
+        return [] 
