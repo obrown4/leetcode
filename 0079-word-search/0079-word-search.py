58407f406dfdaaeb944c81ccfa819299d8a7a238
Owen Brown <90733685+obrown4@users.noreply.github.com>
2024-08-08 22:16:13 -0400

    Time: 5673 ms (17.55%), Space: 16.5 MB (54.58%) - LeetHub

---
+++ 0079-word-search/0079-word-search.py
@@ -0,0 +1,38 @@
+from collections import deque
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        rows, cols = len(board), len(board[0])
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+        visited = set()
+        
+        def inBoard(r, c, i):
+            if 0 <= r < rows and 0 <= c < cols and i < len(word) and board[r][c] == word[i]:
+                return True
+            return False
+
+        def found(i):
+            return i == len(word) - 1
+        
+        def dfs(r: int, c: int, idx: int) -> bool:
+            if not inBoard(r, c, idx) or (r, c) in visited:
+                return False
+            if found(idx):
+                return True
+            
+            visited.add((r, c))
+
+            for x, y in directions:
+                if dfs(x + r, y + c, idx + 1):
+                    return True
+            
+            visited.remove((r, c))
+            return False
+
+
+        
+        for r in range(rows):
+            for c in range(cols):
+                if board[r][c] == word[0] and dfs(r, c, 0):
+                    return True
+        return False
+
