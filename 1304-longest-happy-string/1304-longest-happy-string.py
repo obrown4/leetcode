b331bc8f793bf6a0b0f8cedcaff8865bf672f4be
Owen Brown <90733685+obrown4@users.noreply.github.com>
2024-09-03 11:00:22 -0400

    Time: 42 ms (13.7%), Space: 16.5 MB (93.7%) - LeetHub

--- 1304-longest-happy-string/1304-longest-happy-string.py
+++ 1304-longest-happy-string/1304-longest-happy-string.py
@@ -1,25 +1,43 @@
+import heapq
 class Solution:
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
+        # happy = only a, b, c
+
+        # plan
+        # first greatest letter count
+        # at each letter if possible place 2
+        # iterate in range of the sum of the letters
+        # at each position append greatest letter and then smaller letter 
+        # if there are no aux letters left break the loop return the string
+
         heap = []
         if a > 0:
-            heappush(heap, (-a, 'a'))
+            heapq.heappush(heap, (-a, 'a'))
         if b > 0:
-            heappush(heap, (-b, 'b'))
+            heapq.heappush(heap, (-b, 'b'))
         if c > 0:
-            heappush(heap, (-c, 'c'))
-        res = ""
+            heapq.heappush(heap, (-c, 'c'))
+        
+        happy_string = []
+
         while heap:
-            count, char = heappop(heap)
-            if len(res) >= 2 and res[-1] == res[-2] == char:
+            val, char = heapq.heappop(heap)
+            if len(happy_string) >= 2 and happy_string[-1] == happy_string[-2] == char:
                 if not heap:
                     break
-                nxt_cnt, nxt_char = heappop(heap)
-                res += nxt_char
-                if nxt_cnt + 1 < 0:
-                    heappush(heap, (nxt_cnt + 1, nxt_char))
-                heappush(heap, (count, char))
+                val_pad, char_pad = heapq.heappop(heap)
+                
+                happy_string.append(char_pad)
+
+                if val_pad + 1 < 0:
+                    heapq.heappush(heap, (val_pad + 1, char_pad))
+                heapq.heappush(heap, (val, char))
             else:
-                res += char
-                if count + 1 < 0:
-                    heappush(heap, (count + 1, char))
-        return res
+                happy_string.append(char)
+                if val + 1 < 0:
+                    heapq.heappush(heap, (val + 1, char))
+        return "".join(happy_string)
+
+
+
+ 
