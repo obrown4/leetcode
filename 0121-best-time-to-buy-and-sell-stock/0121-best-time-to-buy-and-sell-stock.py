d76423aa794da11e7571ad3108054f0c619863d7
Owen Brown <90733685+obrown4@users.noreply.github.com>
2024-10-19 13:12:44 -0400

    Time: 109 ms (100%), Space: 18.3 MB (99.99%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,11 +1,15 @@
-class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+
         min_buy = float('inf')
-        maxP = 0
+        profit = 0
 
         for p in prices:
             min_buy = min(min_buy, p)
 
-            maxP = max(maxP, p - min_buy)
-        return maxP
-
+            profit = max(profit, p - min_buy)
+        return profit
